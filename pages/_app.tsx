778bb3a10edcf186391b9f6289c534583aca17e5
RicardoPBarbosa <ricardopbarbosaa@gmail.com>
2021-07-18 19:21:14 +0100

    Cleanup, tailwind, eslint, typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import { FC, ReactElement } from 'react'
+import { AppProps } from 'next/app'
+
+import '../styles/globals.css'
+
+const App: FC<AppProps> = ({ Component, pageProps }): ReactElement => {
+  return <Component {...pageProps} />
+}
+
+export default App
---
+++ pages/api/hello.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const hello = (_: NextApiRequest, res: NextApiResponse): void => {
+  res.statusCode = 200
+  res.json({ name: 'John Doe' })
+}
+
+export default hello
