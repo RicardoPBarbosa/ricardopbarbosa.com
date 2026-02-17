966adf4c673949837f558f3d1e15c2052d58a241
Ricardo Barbosa <ricardopbarbosaa@gmail.com>
2026-02-17 13:28:45 +0000

    update

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react'
 
 const Body = ({ children }: { children: ReactNode }): JSX.Element => (
-  <p className="font-medium text-gray-600 font-body md:text-lg">{children}</p>
+  <p className="font-medium text-gray-600 whitespace-pre-line font-body md:text-lg">
+    {children}
+  </p>
 )
 
 export default Body
